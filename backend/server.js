fd181c0ff209f2773df57636aa682945d92d53cd
mafndi <mafndi2001@gmail.com>
2022-07-14 02:34:29 +0700

    Accepting Body Data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require('express')
+console.log("Hello Azka Alfani")
+const express = require("express");
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
-app.get(`/api/goals`, (req, res)) {
-    res.send(`Get Goals`)
-}
-app.listen(port, () => console.log(`server started on port ${port}`))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes.js'))
